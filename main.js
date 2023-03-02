7cb5f9b952dde7fefe6f11c62f161fd147def278
sol <caceressol@hotmail.com.ar>
2023-03-02 16:06:14 -0300

    Se agrego la funcionalidad del menu hamburguesa para pantallas mobile

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-const nav = document.querySelector("#nav");
+const nav = document.querySelector("#navegador");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
 
-
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+cerrar.addEventListener("click", () => {
+    nav.classList.toggle("visible");
+})
